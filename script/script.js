ee75b263f1f94d17fc00b7eb0184244046e4df23
Mykhailo Matsehora <m.matsehora@pin-up.team>
2020-07-05 17:01:50 +0300

    finish lesson20

--- script/script.js
+++ script/script.js
@@ -41,24 +41,31 @@ window.addEventListener('DOMContentLoaded', function(){
         
     }
 
-    countTimer('5 july 2020');
+    countTimer('6 july 2020');
 
     //Menu
     const toggleMenu = () =>{
         const btnMenu = document.querySelector('.menu'),
-              menu = document.querySelector('menu'),
-              closeBtn = document.querySelector('.close-btn'),
-              menuItems = menu.querySelectorAll('ul>li');
+              menu = document.querySelector('menu');
 
         const handlerMenu = () => {
             menu.classList.toggle('active-menu');
         }
 
+        menu.addEventListener('click', (event) => {
+            let target = event.target;
+
+            if (target.classList.contains('close-btn')) {
+                handlerMenu();
+            } else {
+                target = event.target.closest('menu > ul > li > a');
+                if (target) {
+                    handlerMenu();
+                }
+            }
+        });
+
         btnMenu.addEventListener('click', handlerMenu);
-
-        closeBtn.addEventListener('click', handlerMenu);
-
-        menuItems.forEach((item) => item.addEventListener('click', handlerMenu));
     }
 
     toggleMenu();
@@ -68,7 +75,6 @@ window.addEventListener('DOMContentLoaded', function(){
     const togglePopup = () => {
         const popup = document.querySelector('.popup'),
               popupBtn = document.querySelectorAll('.popup-btn'),
-              popupClose = document.querySelector('.popup-close'),
               screenWidth = document.documentElement.clientWidth;
         
         const fadeIn = () => {
@@ -92,10 +98,58 @@ window.addEventListener('DOMContentLoaded', function(){
             }  
         });
 
-        popupClose.addEventListener('click', () =>{
-            popup.style.display = 'none';
+        popup.addEventListener('click', (event) => {
+            let target = event.target;
+
+            if (target.classList.contains('popup-close')) {
+                popup.style.display = 'none';
+                popup.style.opacity = '0';
+            } else {
+                target = target.closest('.popup-content');
+            
+                if (!target) {
+                    popup.style.display = 'none';
+                    popup.style.opacity = '0';
+                }
+            }
+           
         });
     }
 
     togglePopup();
+
+    //Tabs
+    
+    const tabs = () => {
+        const tabHeader = document.querySelector('.service-header'),
+              tab = tabHeader.querySelectorAll('.service-header-tab'),
+              tabContent = document.querySelectorAll('.service-tab');
+        
+        const toggleTabContent = (index) => {
+            for (let i = 0; i<tabContent.length; i++) {
+                if (index === i) {
+                    tab[i].classList.add('active');
+                    tabContent[i].classList.remove('d-none');
+                } else {
+                    tab[i].classList.remove('active');
+                    tabContent[i].classList.add('d-none');
+                }
+            }
+        } 
+        
+        tabHeader.addEventListener('click', (event) => {
+            let target = event.target;
+            target = target.closest('.service-header-tab');
+
+            if (target) {
+                tab.forEach((item,i) => {
+                    if (item === target) {
+                        toggleTabContent(i);
+                    }
+                });
+            }
+        });
+    }   
+
+    tabs();
 });
