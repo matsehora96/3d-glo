c0aa15cda0de48297cd067ae7bf6f9fd4e140aae
Mykhailo Matsehora <m.matsehora@pin-up.team>
2020-07-14 15:52:12 +0300

    finish lesson26:

--- script/script.js
+++ script/script.js
@@ -336,10 +336,34 @@ window.addEventListener('DOMContentLoaded', function(){
             loadMessage = 'Загрузка...',
             successMessage = 'Спасибо! Мы скоро с вами свяжемся';
         
-        const form = document.getElementById('form1');
+        const form = document.getElementById('form1'),
+            formPopup = document.getElementById('form3'),
+            formQuestion = document.getElementById('form2'),
+            formInput = form.querySelectorAll('input'),
+            formPopupInput = formPopup.querySelectorAll('input'),
+            formQuestionInput = formQuestion.querySelectorAll('input'),
+            inputPhone = document.querySelectorAll('.form-phone'),
+            userName = document.querySelectorAll('[name="user_name"]'),
+            formMessage = document.getElementById('form2-message');
 
         const statusMessage = document.createElement('div');
-        statusMessage.style.cssText = 'font-size: 2rem;';
+        statusMessage.style.cssText = 'font-size: 2rem; color: white;';
+
+        inputPhone.forEach((item) =>{
+            item.addEventListener('input', () =>{
+                item.value = item.value.replace(/\D/g, '');  
+            });
+        });
+
+        userName.forEach((item) =>{
+            item.addEventListener('input', () =>{
+                item.value = item.value.replace(/([0-9a-zA-Z])/g, '');  
+            });
+        });
+
+        formMessage.addEventListener('input', () => {
+            formMessage.value = formMessage.value.replace(/([0-9a-zA-Z])/g, '');
+        });
  
         form.addEventListener('submit', (event) => {
             event.preventDefault();
@@ -358,6 +382,58 @@ window.addEventListener('DOMContentLoaded', function(){
             }, (error) => {
                 statusMessage.textContent = errorMessage;
                 console.error(error);
+            });
+
+            formInput.forEach(item => {
+                item.value = '';
+            });
+        });
+
+        formPopup.addEventListener('submit', (event) => {
+            event.preventDefault();
+            formPopup.appendChild(statusMessage);
+            statusMessage.textContent = loadMessage;
+            const formData = new FormData(formPopup);
+
+            let body = {};
+
+            formData.forEach((val, key) => {
+                body[key] = val;
+            });
+
+            postData(body, () => {
+                statusMessage.textContent = successMessage;
+            }, (error) => {
+                statusMessage.textContent = errorMessage;
+                console.error(error);
+            });
+
+            formPopupInput.forEach(item => {
+                item.value = '';
+            });
+        });
+
+        formQuestion.addEventListener('submit', (event) => {
+            event.preventDefault();
+            formQuestion.appendChild(statusMessage);
+            statusMessage.textContent = loadMessage;
+            const formData = new FormData(formQuestion);
+
+            let body = {};
+
+            formData.forEach((val, key) => {
+                body[key] = val;
+            });
+
+            postData(body, () => {
+                statusMessage.textContent = successMessage;
+            }, (error) => {
+                statusMessage.textContent = errorMessage;
+                console.error(error);
+            });
+
+            formQuestionInput.forEach(item => {
+                item.value = '';
             });
         });
 
